9fb7bff31c40e0033792c9a2401700199a748421
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-11 22:03:33 -0300

    Configurando Spring MVC Controllers

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
