f456327378449b25ecf4858829752ceec10760fd
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-11 22:29:04 -0300

    Mostrando a Lista de Autores

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookController {
 
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @RequestMapping("/books")
     public String getBooks(Model model) {
